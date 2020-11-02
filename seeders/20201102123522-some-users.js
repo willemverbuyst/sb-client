f8b087372f5b01f6bba6aa50afcbae22e4f3d22c
Willem Verbuyst <willemverbuyst@gmail.com>
2020-11-02 13:39:41 +0100

    Add seeder for users

---
+++ seeders/20201102123522-some-users.js
@@ -0,0 +1,42 @@
+'use strict';
+const bcrypt = require('bcrypt');
+const { SALT_ROUNDS } = require('../config/constants');
+
+module.exports = {
+  up: async (queryInterface, Sequelize) => {
+    return queryInterface.bulkInsert(
+      'users',
+      [
+        {
+          userName: 'user_p',
+          firstName: 'Pascal',
+          lastName: 'Duin',
+          email: 'pascal@pascal.com',
+          password: bcrypt.hashSync('pascal123', SALT_ROUNDS),
+          phoneNumber: '0612345678',
+          admin: true,
+          totaalToto: true,
+          createdAt: new Date(),
+          updatedAt: new Date(),
+        },
+        {
+          userName: 'user_r',
+          firstName: 'Rick',
+          lastName: 'Wolt',
+          email: 'rick@rick.com',
+          password: bcrypt.hashSync('rick123', SALT_ROUNDS),
+          phoneNumber: '0612345678',
+          admin: false,
+          totaalToto: true,
+          createdAt: new Date(),
+          updatedAt: new Date(),
+        },
+      ],
+      {}
+    );
+  },
+
+  down: async (queryInterface, Sequelize) => {
+    return queryInterface.bulkDelete('users', null, {});
+  },
+};
