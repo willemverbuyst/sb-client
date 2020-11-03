2c7329cb891cf3ebc1257b311bbaf26f4c650f51
Willem Verbuyst <willemverbuyst@gmail.com>
2020-11-03 16:59:01 +0100

    Use getfixtures to seed fixtures table

--- index.js
+++ index.js
@@ -8,10 +8,15 @@ const authRouter = require('./routers/auth');
 const app = express();
 const bodyParserMiddleWare = express.json();
 
-/* Call getTeams once, to get all the teams and seed the fav_table */
+/* Call getTeams once, to get all the teams and seed the favteam_table */
 /* timestamp: 02-11-2020 16:27 */
 // const teams = require('./api/bc_favteams');
 // teams.getTeams();
+
+/* Call getTFixtures to get all the fixtures and seed the fixtures_table */
+/* timestamp: 03-11-2020 17:00 */
+const fixtures = require('./api/bc_fixtures');
+fixtures.getFixtures();
 
 app.use(loggerMiddleWare('dev'));
 app.use(bodyParserMiddleWare);
