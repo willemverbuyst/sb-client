4e5798a45e5fcac3bc1b4d46f26f0b4772719134
Willem Verbuyst <willemverbuyst@gmail.com>
2020-11-04 05:14:08 +0100

    Create route to get fixture by id

--- routers/fixtures.js
+++ routers/fixtures.js
@@ -1,17 +1,26 @@
 const { Router } = require('express');
 const authMiddleware = require('../auth/authMiddleware');
 const Fixture = require('../models').fixture;
-const Team = require('../models').team;
 
 const router = new Router();
 
 router.get('/', async (_req, res) => {
   try {
-    const fixtures = await Fixture.findOne({
-      where: { id: 573165 },
-    });
+    const fixtures = await Fixture.findAll();
 
     return res.status(200).send(fixtures);
+  } catch (error) {
+    return res.status(400).send({ message: 'Something went wrong, sorry' });
+  }
+});
+
+router.get('/:id', async (req, res) => {
+  const { id } = req.params;
+
+  try {
+    const fixture = await Fixture.findOne({ where: { id } });
+
+    return res.status(200).send(fixture);
   } catch (error) {
     return res.status(400).send({ message: 'Something went wrong, sorry' });
   }
