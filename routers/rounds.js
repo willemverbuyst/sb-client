107f5bfc3bf0262541a064de79721fc0ddef517c
Willem Verbuyst <willemverbuyst@gmail.com>
2020-11-04 16:03:33 +0100

    Get current round with predictions for user

--- routers/rounds.js
+++ routers/rounds.js
@@ -1,38 +1,32 @@
 const { Router } = require('express');
 const authMiddleware = require('../auth/authMiddleware');
 const Fixture = require('../models').fixture;
+const Prediction = require('../models').prediction;
 const lastMonday = require('../utils/helper-functions');
 const { Op } = require('sequelize');
 
 const router = new Router();
 
-router.get('/:id', authMiddleware, async (req, res) => {
+router.get('/current/users/:id', authMiddleware, async (req, res) => {
   const { id } = req.params;
-  if (id === 'current') {
-    try {
-      const timeStampNow = lastMonday();
-      const fixtures = await Fixture.findAll({
-        where: {
-          eventTimeStamp: {
-            [Op.between]: [timeStampNow, timeStampNow + 7 * 24 * 60 * 60],
-          },
+
+  // check if the user is the same one as the one who is logged in
+  if (req.user.id !== +id)
+    res.status(401).send({ message: 'You are not allowed to see this data' });
+
+  try {
+    const timeStampNow = lastMonday();
+    const fixtures = await Fixture.findAll({
+      where: {
+        eventTimeStamp: {
+          [Op.between]: [timeStampNow, timeStampNow + 7 * 24 * 60 * 60],
         },
-      });
-      res.status(200).send(fixtures);
-    } catch (error) {
-      return res.status(400).send({ message: 'Something went wrong, sorry' });
-    }
-  } else {
-    try {
-      const fixtures = await Fixture.findAll({
-        where: {
-          round: `Regular Season - ${id}`,
-        },
-      });
-      return res.status(200).send(fixtures);
-    } catch (error) {
-      return res.status(400).send({ message: 'Something went wrong, sorry' });
-    }
+      },
+      include: { model: Prediction, where: { userId: id }, required: false },
+    });
+    res.status(200).send(fixtures);
+  } catch (error) {
+    return res.status(400).send({ message: 'Something went wrong, sorry' });
   }
 });
 
