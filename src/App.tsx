0cc70ace70d4c78ca7cc2a499e72256a6f8a343a
Willem Verbuyst <willemverbuyst@gmail.com>
2020-11-13 17:01:32 +0100

    Set up header

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,10 @@
 import React from 'react';
+import Header from './Components/Header/index'
 
 function App() {
   return (
     <div className="App">
-     Hello
+      <Header/>
     </div>
   );
 }
