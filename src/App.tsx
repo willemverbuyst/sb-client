9dfc433276e2af6c7ec9043b110056dea01f7970
Willem Verbuyst <willemverbuyst@gmail.com>
2020-11-13 17:09:26 +0100

    Customize colors ui theme

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,16 @@
 import React from 'react';
+import {MuiThemeProvider} from '@material-ui/core/styles'
+import IceBlueGold from './ui/theme';
 import Header from './Components/Header'
 import LogIn from './Pages/LogIn'
 
+
 function App() {
   return (
-    <div className="App">
+    <MuiThemeProvider theme={IceBlueGold}>
       <Header/>
       <LogIn/>
-    </div>
+      </MuiThemeProvider>
   );
 }
 
---
+++ src/ui/theme/index.ts
@@ -0,0 +1,16 @@
+import { createMuiTheme } from '@material-ui/core/styles';
+
+declare module '@material-ui/core/styles/createMuiTheme' {
+  interface ThemeOptions {
+    themeName?: string; // optional
+  }
+}
+
+const palette = {
+  primary: { main: '#1e5eb1' },
+  secondary: { main: '#EA9C3B' },
+};
+
+const themeName = 'IceBlueGold';
+
+export default createMuiTheme({ palette, themeName });
