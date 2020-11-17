a0393609f54816b376f363c251629009a7d8eb3b
Willem Verbuyst <willemverbuyst@gmail.com>
2020-11-17 17:17:49 +0100

    Create action for current round

---
+++ src/store/Voorspellingen/actions.ts
@@ -0,0 +1,47 @@
+import { apiUrl } from '../../config/constants';
+import axios from 'axios';
+import { Dispatch } from 'redux';
+import {
+  CURRENT_ROUND_FETCHED,
+  CurrentRoundFetched,
+  CurrentRound,
+} from './types';
+import { appLoading, appDoneLoading, setMessage } from '../appState/actions';
+import { GetState } from '../types';
+
+const currentRoundFetched = (
+  currentRound: CurrentRound[]
+): CurrentRoundFetched => {
+  return {
+    type: CURRENT_ROUND_FETCHED,
+    currentRound,
+  };
+};
+
+export const fetchCurrentRound = () => async (
+  dispatch: Dispatch,
+  getState: GetState
+) => {
+  if (!getState().voorspellingenState.currentRound) {
+    dispatch(appLoading());
+    try {
+      const token = localStorage.getItem('user_token');
+      const response = await axios.get(`${apiUrl}/rounds/current`, {
+        headers: { Authorization: `Bearer ${token}` },
+      });
+      const round = response.data;
+
+      dispatch(currentRoundFetched(round));
+      dispatch(appDoneLoading());
+    } catch (error) {
+      if (error.response) {
+        console.log(error.response.data.message);
+        dispatch(setMessage('error', error.response.data.message));
+      } else {
+        console.log(error.message);
+        dispatch(setMessage('error', error.message));
+      }
+      dispatch(appDoneLoading());
+    }
+  }
+};
