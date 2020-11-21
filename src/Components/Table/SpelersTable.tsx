b37d74783a4008a045d3512751932e8ea716eb3a
Willem Verbuyst <willemverbuyst@gmail.com>
2020-11-21 09:01:29 +0100

    Connect columns to admin status

--- src/Components/Table/SpelersTable.tsx
+++ src/Components/Table/SpelersTable.tsx
@@ -1,4 +1,6 @@
 import React from 'react';
+import { useSelector } from 'react-redux';
+import { selectUser } from '../../store/user/selectors'
 import { makeStyles } from '@material-ui/core/styles';
 import { 
   Avatar,
@@ -18,10 +20,16 @@ const useStyles = makeStyles((theme) => ({
     height: 30,
     width: 30,
   },
-  check: {
+  checkToto: {
     color: theme.palette.primary.main,
+  },
+  checkAdmin: {
+    color: theme.palette.secondary.main,
   }
 }));
+
+const sortTable = (arr: Player[]): Player[] => [...arr]
+  .sort((player1, player2) => player1.userName.toLowerCase().localeCompare(player2.userName.toLowerCase()))
 
 type Prop = {
   players: Player[]
@@ -29,49 +37,73 @@ type Prop = {
 
 export default function SpelersTable({players}: Prop) {
   const classes = useStyles();
+  const user = useSelector(selectUser);
 
   return (
     <TableContainer>
       <Table aria-label="simple table">
         <TableHead>
           <TableRow>
-            <TableCell align="center">Admin</TableCell>
-            <TableCell align="left">UserName</TableCell>
+            <TableCell align="center">admin</TableCell>
+            <TableCell align="left">user name</TableCell>
             <TableCell align="left">team</TableCell>
-            <TableCell align="left">firstName</TableCell>
-            <TableCell align="left">lastName</TableCell>
-            <TableCell align="left">telefoon</TableCell>
-            <TableCell align="center">totaalToto</TableCell>
+            <TableCell align="left">naam</TableCell>
+            { user && user.admin ? <TableCell align="left">achternaam</TableCell> : null }
+            <TableCell align="center">totaal-toto</TableCell>
+            { user && user.admin ? <TableCell align="left">telefoon</TableCell> : null }
+            <TableCell align="left">email</TableCell>
           </TableRow>
         </TableHead>
         <TableBody>
-          {players.map((player, i) => (
+          {sortTable(players).map((player, i) => (
+
             <TableRow key={i}>
-              <TableCell align="center">
-                <Checkbox
-                  checked={player.admin}
-                  name="checkedB"
-                  color="primary"
-                />
-              </TableCell>
+              { user && user.admin ?  
+                <TableCell align="center">
+                  <Checkbox
+                    checked={player.admin}
+                    name="checkedB"
+                    color="primary"
+                  />
+                </TableCell>
+              :    
+              <TableCell className={classes.checkAdmin} align="center">
+                {player.admin ? <Check/> : null}
+              </TableCell> 
+              }
+             
               <TableCell align="left">
                 {player.userName}
               </TableCell>
+
               <TableCell align="left">
                 <Avatar className={classes.avatar} alt={player.team.name} src={player.team.logo} />
               </TableCell>
+
               <TableCell align="left">
                 {player.firstName}
               </TableCell>
-              <TableCell align="left">
-                {player.lastName}
-              </TableCell>
-              <TableCell align="left">
-                {player.phoneNumber}
-              </TableCell>
-              <TableCell className={classes.check} align="center">
+
+              { user && user.admin ?  
+                <TableCell align="left">
+                  {player.lastName}
+                </TableCell>
+              : null }
+
+              <TableCell className={classes.checkToto} align="center">
                 {player.totaalToto ? <Check/> : null}
               </TableCell> 
+
+              { user && user.admin ?  
+                <TableCell align="left">
+                  {player.phoneNumber}
+                </TableCell>
+              : null }
+
+              <TableCell align="left">
+                {player.email}
+              </TableCell>
+              
             </TableRow>
           ))}
         </TableBody>
