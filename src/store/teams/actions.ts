0adffd3e14bf37f9997d094cca0781f03ca9e5f0
Willem Verbuyst <willemverbuyst@gmail.com>
2020-12-04 18:22:00 +0100

    Test fetchAllTeams action

--- src/store/teams/actions.ts
+++ src/store/teams/actions.ts
@@ -26,29 +26,29 @@ export const removeAllTeams = (): RemoveAllTeams => {
 
 export const fetchAllTeams = () => async (
   dispatch: Dispatch,
-  getState: GetState
+  _getState: GetState
 ) => {
-  if (!getState().teamsState.teams) {
-    dispatch(appLoading());
-    try {
-      const token = localStorage.getItem('user_token');
-      const response = await axios.get(`${apiUrl}/teams`, {
-        headers: { Authorization: `Bearer ${token}` },
-      });
-      const teams = response.data;
+  // if (!getState().teamsState.teams) {
+  dispatch(appLoading());
+  try {
+    const token = localStorage.getItem('user_token');
+    const response = await axios.get(`${apiUrl}/teams`, {
+      headers: { Authorization: `Bearer ${token}` },
+    });
+    const teams = response.data;
 
-      dispatch(allTeamsFetched(teams));
-      dispatch(appDoneLoading());
-    } catch (error) {
-      if (error.response) {
-        console.log(error.response.data.message);
-        dispatch(setMessage('error', error.response.data.message));
-      } else {
-        console.log(error.message);
-        dispatch(setMessage('error', error.message));
-      }
-      dispatch(appDoneLoading());
+    dispatch(allTeamsFetched(teams));
+    dispatch(appDoneLoading());
+  } catch (error) {
+    if (error.response) {
+      console.log(error.response.data.message);
+      dispatch(setMessage('error', error.response.data.message));
+    } else {
+      console.log(error.message);
+      dispatch(setMessage('error', error.message));
     }
+    dispatch(appDoneLoading());
+    // }
   }
 };
 
--- src/store/teams/tests/actions.test.ts
+++ src/store/teams/tests/actions.test.ts
@@ -1,3 +1,4 @@
+import axios from 'axios';
 import { ITeam } from '../../../models/toto.models';
 import {
   allTeamsFetched,
@@ -11,6 +12,9 @@ import {
   AllTeamsFetched,
   RemoveAllTeams,
 } from '../types';
+import { appLoading, appDoneLoading } from '../../appState/actions';
+
+const mockAxios = axios as jest.Mocked<typeof axios>;
 
 describe('#teamsState', () => {
   describe('#allTeamsFetched w/ teams', () => {
@@ -30,15 +34,40 @@ describe('#teamsState', () => {
       expect(allTeamsFetched(teams).teams.length).toBe(1);
     });
   });
+  describe('#removeAllTeams', () => {
+    const expected: RemoveAllTeams = {
+      type: REMOVE_ALL_TEAMS,
+    };
+    test('returns an action w/ type REMOVE_ALL_TEAMS and no payload', () => {
+      expect(removeAllTeams()).toEqual(expected);
+      expect(removeAllTeams().type).toBe(expected.type);
+    });
+  });
 });
 
-describe('#removeAllTeams', () => {
-  const expected: RemoveAllTeams = {
-    type: REMOVE_ALL_TEAMS,
-  };
-  test('returns an action w/ type REMOVE_ALL_TEAMS and no payload', () => {
-    expect(removeAllTeams()).toEqual(expected);
-    expect(removeAllTeams().type).toBe(expected.type);
+describe('#fetchAllTeams', () => {
+  it('calls axios and returns teams', async () => {
+    const teams: ITeam[] = [
+      {
+        id: 1,
+        name: 'test_name',
+        logo: 'test_logo',
+      },
+    ];
+    const dispatch = jest.fn();
+    const getState = jest.fn();
+    const response = { data: teams };
+
+    mockAxios.get.mockImplementationOnce(() => Promise.resolve(response));
+
+    await fetchAllTeams()(dispatch, getState);
+
+    expect(mockAxios.get).toHaveBeenCalledTimes(1);
+
+    expect(dispatch).toHaveBeenCalledWith(appLoading());
+    expect(dispatch).toHaveBeenCalledWith(allTeamsFetched(teams));
+    expect(dispatch).toHaveBeenCalledWith(appDoneLoading());
+    expect(dispatch).toHaveBeenCalledTimes(3);
   });
 });
 
