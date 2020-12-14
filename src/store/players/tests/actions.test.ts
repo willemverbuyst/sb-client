5577839ef4d26537b7e63a4b761ad51af75b9e54
Willem Verbuyst <willemverbuyst@gmail.com>
2020-12-14 07:20:09 +0100

    Test playerProfileFetched action

--- src/store/players/tests/actions.test.ts
+++ src/store/players/tests/actions.test.ts
@@ -1,5 +1,9 @@
-import { IPlayer } from '../../../models/player.model';
-import { addNewPlayer, allPlayersFetched } from '../actions';
+import { IPlayer, IPlayerProfile } from '../../../models/player.model';
+import {
+  addNewPlayer,
+  allPlayersFetched,
+  playerProfileFetched,
+} from '../actions';
 import {
   ADD_NEW_PLAYER,
   ALL_PLAYERS_FETCHED,
@@ -64,6 +68,34 @@ describe('#playersState', () => {
       expect(allPlayersFetched(players)).toEqual(expected);
       expect(allPlayersFetched(players).players.length).toBe(1);
       expect(allPlayersFetched(players).players).not.toBe(undefined);
+    });
+  });
+  describe('#playerProfileFetched w/ profile', () => {
+    const playerProfile: IPlayerProfile = {
+      admin: false,
+      email: 'test@test.com',
+      firstName: 'test_player',
+      id: 1,
+      lastName: 'tst_player',
+      phoneNumber: '123',
+      team: {
+        id: 1,
+        logo: 'test_logo',
+        name: 'test_name',
+      },
+      totaalToto: true,
+      userName: 'TEST',
+      pastFixturesWithScores: null,
+    };
+    const expected: PlayerProfileFetched = {
+      type: PLAYER_PROFILE_FETCHED,
+      playerProfile,
+    };
+    test('returns an action w/ type PLAYER_PROFILE_FETCHED and a profile as payload', () => {
+      expect(playerProfileFetched(playerProfile)).toEqual(expected);
+      expect(playerProfileFetched(playerProfile)?.playerProfile.firstName).toBe(
+        'test_player'
+      );
     });
   });
 });
