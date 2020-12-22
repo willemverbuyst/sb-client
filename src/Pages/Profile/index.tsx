2abb9291b85b3cc18f6f50a15541c8cebe58863a
Willem Verbuyst <willemverbuyst@gmail.com>
2020-12-22 05:21:45 +0100

    Clean up profile component

--- src/Pages/Profile/index.tsx
+++ src/Pages/Profile/index.tsx
@@ -1,12 +1,11 @@
 import React, { useEffect, useState } from 'react';
 import { useHistory } from "react-router-dom";
-import { useDispatch, useSelector } from 'react-redux';
-import { selectScores, selectToken } from '../../store/user/selectors'
+import { useSelector } from 'react-redux';
+import { selectToken } from '../../store/user/selectors'
 import { makeStyles, Theme } from '@material-ui/core/styles';
 import { 
   Box, 
   Button, 
-  Divider, 
   Grid, 
   Typography 
 } from '@material-ui/core';
@@ -14,8 +13,6 @@ import { selectAppLoading } from '../../store/appState/selectors';
 import ProgressLinear from '../../Components/Progress/ProgressLinear';
 import ChangePasswordForm from '../../Components/Form/ChangePasswordForm';
 import EditProfileForm from '../../Components/Form/EditProfileForm';
-import { fetchUserScores } from '../../store/user/actions';
-import ScoresStackedChart from '../../Components/Chart/ScoresStackedChart';
 
 const useStyles = makeStyles((theme: Theme) => ({
   title: {
@@ -45,30 +42,15 @@ export default function Profile() {
   const classes = useStyles();
   const token = useSelector(selectToken);
   const history = useHistory();
-  const dispatch = useDispatch();
   const isLoading = useSelector(selectAppLoading);
-  const scores = useSelector(selectScores);
-  const [editProfile, setEditProfile] = useState(false);
-  const [changePassword, setChangePassword] = useState(false);
+  const [editProfile, setEditProfile] = useState(true);
 
   useEffect(() => {
     if (!token) history.push("/login");
   });
 
-  useEffect(() => {
-    if (!scores) {
-      dispatch(fetchUserScores());
-    }
-  });
-
   const handleEditProfile = () => {
-    setChangePassword(false);
     setEditProfile(!editProfile)
-  }
-
-  const handleChangePassword = () => {
-    setEditProfile(false);
-    setChangePassword(!changePassword)
   }
 
   return (
@@ -87,66 +69,20 @@ export default function Profile() {
             disableElevation 
             onClick={handleEditProfile}
           >
-            {!editProfile ? 'EDIT PROFIEL' : 'SLUIT EDIT PROFIEL'}
+            {!editProfile ? 'EDIT PROFIEL' : 'CHANGE PASSWORD'}
           </Button>
-          <Button
-            variant="contained"
-            size="small" 
-            color="secondary"
-            disableElevation 
-            onClick={handleChangePassword}
-            className={classes.passwordBtn}
-          >
-            {!changePassword? 'CHANGE PASSWORD' : 'SLUIT CHANGE PASSWORD'}
-          </Button>  
         </Grid>
       </Grid>
-
-      { scores ? 
-        <>
-          <Grid 
-            item xs={12} 
-            container justify="center" 
-            className={classes.totoRound}
-          >
-            <Typography variant="h4">
-              MIJN SCORES
-            </Typography>
-          </Grid>
-
-          <Grid>
-            <Divider className={classes.divider}/>
-          </Grid>
-          
-          
-          <Grid
-            container
-            direction="row"
-            justify="center"
-            alignItems="center"
-          >
-            <Grid item xs={12} md={6} container justify="center">
-              <ScoresStackedChart userScores={scores}/>
-            </Grid>
-          </Grid>
-        </>  
-      : 
-        <Grid>
-          <Typography variant="overline">
-            Je hebt nog geen scores
-          </Typography>
-        </Grid> 
-      }
 
       { isLoading ?
           <Box className={classes.progress}>
             <ProgressLinear/> 
           </Box>
         : editProfile ? 
-          <EditProfileForm handleSubmit={() => setEditProfile(false)}/>
-        : changePassword ? 
-          <ChangePasswordForm handleSubmit={() => setChangePassword(false)}/>
-        : null }
+          <EditProfileForm handleSubmit={() => setEditProfile(true)}/>
+        : 
+          <ChangePasswordForm handleSubmit={() => setEditProfile(true)}/>
+        }
     </Box>
   )
 }
