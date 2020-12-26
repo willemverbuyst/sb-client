3e426cdd25bbcc3c05774fea7500aacf1773f406
Willem Verbuyst <willemverbuyst@gmail.com>
2020-12-26 11:25:36 +0100

    Test timeFunctions

---
+++ src/utils/tests/timeFuctions.test.ts
@@ -0,0 +1,22 @@
+import {
+  getTimeFromTimeStamp,
+  timeStampFormattedToLocalDate,
+} from '../timeFunctions';
+
+describe('if given timestamp', () => {
+  const timeStampTest = 1610206200;
+
+  test('returns a time (hh:mm)', () => {
+    expect(getTimeFromTimeStamp(timeStampTest)).toBe('16:30');
+  });
+});
+
+describe('if give timestamp', () => {
+  const timeStampTest = 1610206200;
+
+  test('returns a formatted date', () => {
+    expect(
+      timeStampFormattedToLocalDate(timeStampTest).toLocaleUpperCase()
+    ).toBe('ZATERDAG 9 JANUARI 2021');
+  });
+});
--- src/utils/timeFunctions.ts
+++ src/utils/timeFunctions.ts
@@ -10,8 +10,8 @@ const monthsLocal = [
   'september',
   'oktober',
   'november',
-  'december'
-  ]
+  'december',
+];
 
 const monthsLocalShort = [
   'jan',
@@ -25,8 +25,8 @@ const monthsLocalShort = [
   'sep',
   'okt',
   'nov',
-  'dec'
-  ]
+  'dec',
+];
 
 const daysLocal = [
   'zondag',
@@ -36,40 +36,21 @@ const daysLocal = [
   'donderdag',
   'vrijdag',
   'zaterdag',
-]
+];
 
-const daysLocalShort = [
-  'zo',
-  'ma',
-  'di',
-  'wo',
-  'do',
-  'vr',
-  'za',
-]
-
-export const getToday = ():string => {
-  const day = daysLocalShort[+new Date().getDay()]
-  const month = monthsLocalShort[+new Date().getMonth()]
-
-  return `${day} ${new Date().getDate()} ${month}`
-}
-
-export const getTimeNow = ():string => new Date().toLocaleString().slice(11, 19);
-
-export const timeStampFormattedToLocalDate = (timeStamp: number):string => {
-  const date = new Date(timeStamp * 1000);
-  const day = daysLocal[+date.getDay()];
-  const month = monthsLocal[+date.getMonth()];
-  const year = date.getFullYear().toString()
-
-  return `${day} ${date.getDate()} ${month} ${year}`;
-}
-
-export const getTimeFromTimeStamp = (timeStamp: number):string => {
+export const getTimeFromTimeStamp = (timeStamp: number): string => {
   const date = new Date(timeStamp * 1000);
   const hours = date.getHours();
   const minutes = date.getMinutes() === 0 ? '00' : date.getMinutes();
 
   return `${hours}:${minutes}`;
-}
+};
+
+export const timeStampFormattedToLocalDate = (timeStamp: number): string => {
+  const date = new Date(timeStamp * 1000);
+  const day = daysLocal[+date.getDay()];
+  const month = monthsLocal[+date.getMonth()];
+  const year = date.getFullYear().toString();
+
+  return `${day} ${date.getDate()} ${month} ${year}`;
+};
