01d0e9e0f7b7f6d81830cc4a39ce077af5b4fe56
Willem Verbuyst <willemverbuyst@gmail.com>
2020-12-30 11:25:30 +0100

    Update apiUrl for deployment

--- src/config/constants.ts
+++ src/config/constants.ts
@@ -1,6 +1,6 @@
 import * as dotenv from 'dotenv';
 dotenv.config({ path: __dirname + '../../.env' });
 
-export const apiUrl = process.env.API_URL || 'http://localhost:4000';
+export const apiUrl = process.env.REACT_APP_SERVER_URL;
 
 export const DEFAULT_MESSAGE_TIMEOUT = 3000;
