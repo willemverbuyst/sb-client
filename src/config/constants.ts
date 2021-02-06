f9dcb40edbcbe473ba907adac541bac9b5b6be1a
Willem Verbuyst <willemverbuyst@gmail.com>
2021-02-06 06:56:02 +0100

    Update app with _redirects

--- src/config/constants.ts
+++ src/config/constants.ts
@@ -3,4 +3,6 @@ dotenv.config({ path: __dirname + '../../.env' });
 
 export const apiUrl = process.env.REACT_APP_SERVER_URL;
 
+// export const apiUrl = 'http://localhost:4000';
+
 export const DEFAULT_MESSAGE_TIMEOUT = 3000;
