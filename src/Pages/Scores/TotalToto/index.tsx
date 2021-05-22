562a4a21676078cab174224f4cffd8e04f54eec5
Willem Verbuyst <willemverbuyst@gmail.com>
2021-05-22 05:05:04 +0200

    Create TopSection TotalToto

--- src/Pages/Scores/TotalToto/index.tsx
+++ src/Pages/Scores/TotalToto/index.tsx
@@ -1,30 +1,22 @@
-import { Box, Grid, Theme } from '@material-ui/core';
-import { makeStyles } from '@material-ui/core/styles';
+import { Box, Grid } from '@material-ui/core';
 import React, { ReactElement, useEffect } from 'react';
 import { useDispatch, useSelector } from 'react-redux';
 import { useHistory } from 'react-router-dom';
 
-import ButtonComponent from '../../../Components/Button';
 import ScoresBarChart from '../../../Components/Chart/ScoresBarChart';
 import DividerComponent from '../../../Components/Divider';
 import Message from '../../../Components/Message';
 import ProgressComponent from '../../../Components/Progress';
-import PageTitleComponent from '../../../Components/Title/PageTitle';
 import SubTitleComponent from '../../../Components/Title/SubTitle';
 import { selectAppLoading } from '../../../store/appState/selectors';
 import { fetchScoresTotalToto } from '../../../store/scores/actions';
 import { selectTotalToto } from '../../../store/scores/selectors';
 import { UserWithScore } from '../../../store/scores/types';
 import { selectToken } from '../../../store/user/selectors';
-import { topSection } from '../../../ui/sharedClasses';
 import BreadCrumbsSection from './BreadCrumbsSection';
-
-const useStyles = makeStyles((theme: Theme) => ({
-  ...topSection(theme),
-}));
+import TopSection from './TopSection';
 
 const TotalToto: React.FC = (): ReactElement => {
-  const classes = useStyles();
   const dispatch = useDispatch();
   const token = useSelector(selectToken);
   const history = useHistory();
@@ -45,14 +37,9 @@ const TotalToto: React.FC = (): ReactElement => {
     ? [...totalToto].sort((name1, name2) => name1.user.toLowerCase().localeCompare(name2.user.toLowerCase()))
     : [];
 
-  const gotoTotoRound = () => history.push(`/voorspellingen/1/1`);
-
   return (
     <Box>
-      <Grid container className={classes.topSection}>
-        <PageTitleComponent text="Klassement" />
-        <ButtonComponent caption="MIJN VOORSPELLINGEN" color="primary" handleClick={gotoTotoRound} />
-      </Grid>
+      <TopSection />
 
       {isLoading ? (
         <ProgressComponent />
