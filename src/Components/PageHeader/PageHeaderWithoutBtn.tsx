1eb2d5214ceac01ca34c91c88d552a2e4cc63408
Willem Verbuyst <willemverbuyst@gmail.com>
2021-05-22 13:02:57 +0200

    Use PageHeaderWithoutBtn

---
+++ src/Components/PageHeader/PageHeaderWithoutBtn.tsx
@@ -0,0 +1,26 @@
+import { Grid, Theme } from '@material-ui/core';
+import { makeStyles } from '@material-ui/core/styles';
+import React, { ReactElement } from 'react';
+
+import PageTitleComponent from '../../Components/Title/PageTitle';
+import { topSection } from '../../ui/sharedClasses';
+
+const useStyles = makeStyles((theme: Theme) => ({
+  ...topSection(theme),
+}));
+
+interface IProps {
+  text: string;
+}
+
+const PageHeaderWithoutButton: React.FC<IProps> = ({ text }: IProps): ReactElement => {
+  const classes = useStyles();
+
+  return (
+    <Grid container className={classes.topSection}>
+      <PageTitleComponent text={text} />
+    </Grid>
+  );
+};
+
+export default PageHeaderWithoutButton;
