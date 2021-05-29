3dc39e413b3cef1b37808d546c44efd3385c105c
Willem Verbuyst <willemverbuyst@gmail.com>
2021-05-29 07:47:29 +0200

    Create PasswordFieldComponent

--- src/Pages/Profile/EditPassword/EditPasswordForm.tsx
+++ src/Pages/Profile/EditPassword/EditPasswordForm.tsx
@@ -1,4 +1,4 @@
-import { Grid, TextField } from '@material-ui/core';
+import { Grid } from '@material-ui/core';
 import { makeStyles, Theme } from '@material-ui/core/styles';
 import { Alert } from '@material-ui/lab';
 import React, { ReactElement, useState } from 'react';
@@ -6,6 +6,7 @@ import { useDispatch } from 'react-redux';
 import { Link } from 'react-router-dom';
 
 import SubmitButtonComponent from '../../../Components/Button/SubmitButton';
+import PasswordFieldComponent from '../../../Components/Form/PasswordField';
 import { ButtonEvent } from '../../../models/events.model';
 import { changePassword } from '../../../store/user/actions';
 
@@ -51,39 +52,18 @@ const EditPasswordForm: React.FC = (): ReactElement => {
     setPassword2('');
   };
 
+  const updatePassword1 = (event: React.ChangeEvent<HTMLInputElement>) => setPassword1(event.target.value);
+  const updatePassword2 = (event: React.ChangeEvent<HTMLInputElement>) => setPassword2(event.target.value);
+
   return (
     <Grid container justify="center">
       <Grid item xs={12} sm={8} md={6} lg={4} className={classes.paper}>
         <form className={classes.form} noValidate>
-          <TextField
-            variant="outlined"
-            margin="normal"
-            required
-            fullWidth
-            id="password1"
-            label="Password"
-            name="password1"
-            type="password"
-            autoComplete="email"
-            autoFocus
-            value={password1}
-            onChange={(e) => setPassword1(e.target.value)}
-          />
-          <TextField
-            variant="outlined"
-            margin="normal"
-            required
-            fullWidth
-            name="password2"
-            label="Confirm password"
-            type="password"
-            id="password2"
-            value={password2}
-            onChange={(e) => setPassword2(e.target.value)}
-          />
+          <PasswordFieldComponent id="password1" label="Password" value={password1} onChange={updatePassword1} />
+          <PasswordFieldComponent id="password2" label="Password" value={password2} onChange={updatePassword2} />
           <SubmitButtonComponent caption="CHANGE PASSWORD" color="primary" handleClick={submitForm} />
-
           <Link to="/profiel/edit">Edit Profile</Link>
+
           {showAlert ? (
             <Alert onClose={closeAlert} severity="error" variant="outlined" className={classes.margin}>
               Passwords are not the same
