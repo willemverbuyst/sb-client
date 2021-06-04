2e324d91555042677be61713cdcc81233aa492d1
Willem Verbuyst <willemverbuyst@gmail.com>
2021-06-04 07:07:51 +0200

    Refactor fixtures section for predictions player

--- src/Pages/Players/PredictionsPlayer/FixturesSection.tsx
+++ src/Pages/Players/PredictionsPlayer/FixturesSection.tsx
@@ -2,9 +2,8 @@ import { Grid } from '@material-ui/core';
 import { makeStyles, Theme } from '@material-ui/core/styles';
 import React, { ReactElement } from 'react';
 
-import { TotoRound } from '../../../models/toto.models';
+import { IFixtureWithScoreAndPredictions } from '../../../models/toto.models';
 import { content } from '../../../ui/sharedClasses';
-import { calculateIndex } from '../../../utils/parameterFunctions';
 import MatchCard from '../../Sections/Card/MatchCard';
 
 const useStyles = makeStyles((theme: Theme) => ({
@@ -12,27 +11,19 @@ const useStyles = makeStyles((theme: Theme) => ({
 }));
 
 interface IProps {
-  pastFixturesWithScores: TotoRound[];
-  totoronde: string;
-  ronde: string;
+  fixtures: IFixtureWithScoreAndPredictions[];
 }
 
-const FixturesSection: React.FC<IProps> = ({ pastFixturesWithScores, totoronde, ronde }: IProps): ReactElement => {
+const FixturesSection: React.FC<IProps> = ({ fixtures }: IProps): ReactElement => {
   const classes = useStyles();
-  const t = Number(totoronde);
-  const r = Number(ronde);
 
   return (
     <Grid item xs={12} container justify="center" className={classes.content}>
-      {pastFixturesWithScores
-        ? [...pastFixturesWithScores[t - 1][calculateIndex(r)]]
-            .sort((f1, f2) => f1.eventTimeStamp - f2.eventTimeStamp)
-            .map((wedstrijd, i) => (
-              <Grid item key={i} lg={4} md={6} xs={12}>
-                <MatchCard wedstrijdMetVoorspellingen={wedstrijd} display="public" />
-              </Grid>
-            ))
-        : null}
+      {fixtures.map((wedstrijd, i) => (
+        <Grid item key={i} lg={4} md={6} xs={12}>
+          <MatchCard wedstrijdMetVoorspellingen={wedstrijd} display="public" />
+        </Grid>
+      ))}
     </Grid>
   );
 };
--- src/Pages/Players/PredictionsPlayer/index.tsx
+++ src/Pages/Players/PredictionsPlayer/index.tsx
@@ -4,8 +4,11 @@ import { useHistory, useParams } from 'react-router-dom';
 
 import MessageComponent from '../../../Components/Communication/Message';
 import PageHeaderWithButton from '../../../Components/Header/PageHeaderWithBtn';
+import { IFixtureWithScoreAndPredictions } from '../../../models/toto.models';
 import { fetchPlayerProfile } from '../../../store/players/action-creators';
 import { selectPastFixturesWithScoresPlayer, selectUserNamePlayer } from '../../../store/players/selectors';
+import { calculateIndex } from '../../../utils/parameterFunctions';
+import { sortArrayWithObjects } from '../../../utils/sortFunctions';
 import PageContent from '../../Sections/PageContent';
 import FixturesSection from './FixturesSection';
 import PaginationSection from './PaginationSection';
@@ -13,11 +16,14 @@ import PaginationSection from './PaginationSection';
 const PredictionsPlayer: React.FC = (): ReactElement => {
   const dispatch = useDispatch();
   const history = useHistory();
+  const userNamePlayer = useSelector(selectUserNamePlayer);
+  const pastFixturesWithScores = useSelector(selectPastFixturesWithScoresPlayer);
   const { id } = useParams<{ id: string }>();
   const { ronde } = useParams<{ ronde: string }>();
   const { totoronde } = useParams<{ totoronde: string }>();
-  const userNamePlayer = useSelector(selectUserNamePlayer);
-  const pastFixturesWithScores = useSelector(selectPastFixturesWithScoresPlayer);
+
+  const t = Number(totoronde);
+  const r = Number(ronde);
 
   const name = userNamePlayer ? userNamePlayer : 'Speler...';
 
@@ -25,16 +31,24 @@ const PredictionsPlayer: React.FC = (): ReactElement => {
     dispatch(fetchPlayerProfile(Number(id)));
   }, [dispatch, id]);
 
+  const filteredFixtures = pastFixturesWithScores ? [...pastFixturesWithScores[t - 1][calculateIndex(r)]] : null;
+
+  const filteredAndSortedfixtures = filteredFixtures
+    ? sortArrayWithObjects<keyof IFixtureWithScoreAndPredictions, IFixtureWithScoreAndPredictions>('eventTimeStamp')(
+        filteredFixtures,
+      )
+    : null;
+
   const gotoScores = () => history.push(`/spelers/${id}/scores`);
 
   return (
     <PageContent
       loadingText="Speler..."
       content={
-        pastFixturesWithScores ? (
+        pastFixturesWithScores && filteredAndSortedfixtures ? (
           <>
             <PageHeaderWithButton title={name} captionBtn="SCORES" colorBtn="secondary" handleClick={gotoScores} />
-            <FixturesSection pastFixturesWithScores={pastFixturesWithScores} totoronde={totoronde} ronde={ronde} />
+            <FixturesSection fixtures={filteredAndSortedfixtures} />
             <PaginationSection
               totoronde={totoronde}
               ronde={ronde}
