a2071983dd09e55c3c719c163d12bad6cad2d47d
Willem Verbuyst <willemverbuyst@gmail.com>
2021-06-13 05:25:11 +0200

    Strip total toto page

--- src/Components/Chart/BarChart.tsx
+++ src/Components/Chart/BarChart.tsx
@@ -1,6 +1,6 @@
 import 'chartjs-plugin-datalabels';
 
-import { Grid } from '@material-ui/core';
+import { Box, Grid } from '@material-ui/core';
 import * as chartjs from 'chart.js';
 import React, { ReactElement } from 'react';
 import { Bar, ChartData } from 'react-chartjs-2';
@@ -13,17 +13,19 @@ interface IProps {
 
 const BarChart: React.FC<IProps> = ({ chartData, chartOptions, goto }: IProps): ReactElement => {
   return (
-    <Grid container direction="row" justify="center" alignItems="center">
-      <Grid item xs={12} md={6} container justify="center">
-        <Bar
-          data={chartData}
-          options={chartOptions}
-          onElementsClick={(e) => {
-            if (e[0] !== undefined) goto(e[0]._index);
-          }}
-        />
+    <Box mt="4rem">
+      <Grid container direction="row" justify="center" alignItems="center">
+        <Grid item xs={12} md={6} container justify="center">
+          <Bar
+            data={chartData}
+            options={chartOptions}
+            onElementsClick={(e) => {
+              if (e[0] !== undefined) goto(e[0]._index);
+            }}
+          />
+        </Grid>
       </Grid>
-    </Grid>
+    </Box>
   );
 };
 
