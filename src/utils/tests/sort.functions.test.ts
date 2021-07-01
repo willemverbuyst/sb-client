bae8591e5ab3099ab7c6c0a3c6918857bb44cb39
Willem Verbuyst <willemverbuyst@gmail.com>
2021-07-01 06:36:30 +0200

    Add function name to test sort function

--- src/utils/tests/sort.functions.test.ts
+++ src/utils/tests/sort.functions.test.ts
@@ -1,24 +1,26 @@
 import { sortArrayWithObjects } from '../sort.functions';
 
-describe('if given an array with objects and a key', () => {
-  const testArray = [
-    { id: 1, name: 'Xeno' },
-    { id: 3, name: 'Freida' },
-    { id: 2, name: 'Annie' },
-  ];
-  const arraySortedById = [
-    { id: 1, name: 'Xeno' },
-    { id: 2, name: 'Annie' },
-    { id: 3, name: 'Freida' },
-  ];
-  const arraySortedByName = [
-    { id: 2, name: 'Annie' },
-    { id: 3, name: 'Freida' },
-    { id: 1, name: 'Xeno' },
-  ];
+describe('#sortArrayWithObjects', () => {
+  describe('if given an array with objects and a key', () => {
+    const testArray = [
+      { id: 1, name: 'Xeno' },
+      { id: 3, name: 'Freida' },
+      { id: 2, name: 'Annie' },
+    ];
+    const arraySortedById = [
+      { id: 1, name: 'Xeno' },
+      { id: 2, name: 'Annie' },
+      { id: 3, name: 'Freida' },
+    ];
+    const arraySortedByName = [
+      { id: 2, name: 'Annie' },
+      { id: 3, name: 'Freida' },
+      { id: 1, name: 'Xeno' },
+    ];
 
-  test('returns the array with objects sorted by that key', () => {
-    expect(sortArrayWithObjects('id')(testArray)).toEqual(arraySortedById);
-    expect(sortArrayWithObjects('name')(testArray)).toEqual(arraySortedByName);
+    test('returns the array with objects sorted by that key', () => {
+      expect(sortArrayWithObjects('id')(testArray)).toEqual(arraySortedById);
+      expect(sortArrayWithObjects('name')(testArray)).toEqual(arraySortedByName);
+    });
   });
 });
