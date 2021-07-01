ecbf48d58030a0789a4dcd3d82303a9bbdc750a8
Willem Verbuyst <willemverbuyst@gmail.com>
2021-07-01 06:39:51 +0200

    Add function names to tests parameter functions

--- src/utils/tests/parameterFunctions.test.ts
+++ src/utils/tests/parameterFunctions.test.ts
@@ -1,29 +1,35 @@
 import { TOTAL_ROUNDS } from '../../constants/setupGame';
 import { calculateIndex, roundByTotoRound, totoRoundByRound } from '../parameter.functions';
 
-describe('if given a totoRound number', () => {
-  test('returns the first round of that toto round', () => {
-    expect(roundByTotoRound(1)).toBe(1);
-    expect(roundByTotoRound(3)).toBe(7);
-    expect(roundByTotoRound(11)).toBe(31);
+describe('#roundByTotoRound', () => {
+  describe('if given a totoRound number', () => {
+    test('returns the first round of that toto round', () => {
+      expect(roundByTotoRound(1)).toBe(1);
+      expect(roundByTotoRound(3)).toBe(7);
+      expect(roundByTotoRound(11)).toBe(31);
+    });
   });
 });
 
-describe('if given a round number', () => {
-  test('returns the toto round number of that round', () => {
-    expect(totoRoundByRound(1)).toBe(1);
-    expect(totoRoundByRound(13)).toBe(5);
-    expect(totoRoundByRound(8)).toBe(3);
-    expect(totoRoundByRound(TOTAL_ROUNDS)).toBe(11);
+describe('#totoRoundByRound', () => {
+  describe('if given a round number', () => {
+    test('returns the toto round number of that round', () => {
+      expect(totoRoundByRound(1)).toBe(1);
+      expect(totoRoundByRound(13)).toBe(5);
+      expect(totoRoundByRound(8)).toBe(3);
+      expect(totoRoundByRound(TOTAL_ROUNDS)).toBe(11);
+    });
   });
 });
 
-describe('if given a round number', () => {
-  test('returns the index of that round in the array', () => {
-    expect(calculateIndex(1)).toBe(0);
-    expect(calculateIndex(13)).toBe(0);
-    expect(calculateIndex(8)).toBe(1);
-    expect(calculateIndex(9)).toBe(2);
-    expect(calculateIndex(TOTAL_ROUNDS)).toBe(3);
+describe('#calculateIndex', () => {
+  describe('if given a round number', () => {
+    test('returns the index of that round in the array', () => {
+      expect(calculateIndex(1)).toBe(0);
+      expect(calculateIndex(13)).toBe(0);
+      expect(calculateIndex(8)).toBe(1);
+      expect(calculateIndex(9)).toBe(2);
+      expect(calculateIndex(TOTAL_ROUNDS)).toBe(3);
+    });
   });
 });
