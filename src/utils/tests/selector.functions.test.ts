8ae5dc3323a0b8ff725af7d7515a35b66c4e70a4
Willem Verbuyst <willemverbuyst@gmail.com>
2021-07-01 06:37:35 +0200

    Add function name to test selector function

--- src/utils/tests/selector.functions.test.ts
+++ src/utils/tests/selector.functions.test.ts
@@ -1,18 +1,20 @@
 import { getTeamsForSelector } from '../selector.functions';
 
-describe('if given an array with objects', () => {
-  const testArray = [
-    { id: 1, name: 'Xeno', logo: '122' },
-    { id: 3, name: 'Freida', logo: '124' },
-    { id: 2, name: 'Annie', logo: '12' },
-  ];
-  const arrayForSelector = [
-    { id: 2, name: 'Annie' },
-    { id: 3, name: 'Freida' },
-    { id: 1, name: 'Xeno' },
-  ];
+describe('#getTeamsForSelector', () => {
+  describe('if given an array with objects', () => {
+    const testArray = [
+      { id: 1, name: 'Xeno', logo: '122' },
+      { id: 3, name: 'Freida', logo: '124' },
+      { id: 2, name: 'Annie', logo: '12' },
+    ];
+    const arrayForSelector = [
+      { id: 2, name: 'Annie' },
+      { id: 3, name: 'Freida' },
+      { id: 1, name: 'Xeno' },
+    ];
 
-  test('returns array with objects with name and id as keys only, sorted by name', () => {
-    expect(getTeamsForSelector(testArray)).toEqual(arrayForSelector);
+    test('returns array with objects with name and id as keys only, sorted by name', () => {
+      expect(getTeamsForSelector(testArray)).toEqual(arrayForSelector);
+    });
   });
 });
