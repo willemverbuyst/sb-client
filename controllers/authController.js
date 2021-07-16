9fb33355c5b3ba80a774ed99ed96010a92f0894a
Willem Verbuyst <willemverbuyst@gmail.com>
2021-07-16 18:30:51 +0200

    Create query for current round

--- controllers/authController.js
+++ controllers/authController.js
@@ -1,17 +1,10 @@
 const bcrypt = require('bcrypt');
 const catchAsync = require('../utils/catchAsync');
 const AppError = require('../utils/appError');
-const Fixture = require('../models').fixture;
-const Prediction = require('../models').prediction;
 
-const { Op } = require('sequelize');
-const {
-  lastMonday,
-  nextMonday,
-  getTotoRoundNumber,
-} = require('../utils/helper-functions');
 const { toJWT } = require('../auth/jwt');
 const { getUserByEmail } = require('../queries/userQuery');
+const { getCurrentRoundForUser } = require('../queries/roundQuery');
 
 exports.login = catchAsync(async (req, res, next) => {
   const { email, password } = req.body;
@@ -31,53 +24,7 @@ exports.login = catchAsync(async (req, res, next) => {
     );
   }
 
-  const timeStampLastMonday = lastMonday();
-  const timeStampNextMonday = nextMonday();
-
-  const fixturesWithPrediction = await Fixture.findAll({
-    where: {
-      eventTimeStamp: {
-        [Op.between]: [timeStampLastMonday, timeStampNextMonday],
-      },
-      status: 'Not Started',
-    },
-    include: {
-      model: Prediction,
-      where: { userId: user.id },
-      attributes: ['pGoalsAwayTeam', 'pGoalsHomeTeam'],
-      required: false,
-    },
-    raw: true,
-    nest: true,
-  });
-
-  const fixturesWithPredictionAndScore = fixturesWithPrediction.map((fix) => {
-    return {
-      ...fix,
-      score: calcScores(
-        fix.status,
-        { homeTeam: fix.goalsHomeTeam, awayTeam: fix.goalsAwayTeam },
-        {
-          homeTeam: fix.predictions.pGoalsHomeTeam,
-          awayTeam: fix.predictions.pGoalsAwayTeam,
-        },
-      ),
-    };
-  });
-
-  let currentRound = null;
-
-  if (fixturesWithPredictionAndScore.length > 0) {
-    const roundNumber = fixturesWithPredictionAndScore[0].round.slice(-2);
-    const totoRoundNumber = getTotoRoundNumber(roundNumber);
-
-    currentRound = {
-      roundNumber,
-      totoRoundNumber,
-      fixtures: fixturesWithPredictionAndScore,
-    };
-  }
-
+  const currentRound = await getCurrentRoundForUser(user.id);
   const token = toJWT({ userId: user.email });
 
   res.status(200).json({
