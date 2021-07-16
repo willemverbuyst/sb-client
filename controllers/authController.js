3f4c0f4540e7d5e6eda1bf0ab1029caf95aa3e2b
Willem Verbuyst <willemverbuyst@gmail.com>
2021-07-16 06:47:43 +0200

    Create authController

---
+++ controllers/authController.js
@@ -0,0 +1,16 @@
+const catchAsync = require('../utils/catchAsync');
+const AppError = require('../utils/appError');
+
+exports.login = catchAsync(async (req, res, next) => {
+  const { email, password } = req.body;
+
+  if (!email || !password) {
+    return next(new AppError('Please provide email and password'), 400);
+  }
+
+  const user = await User.findOne({ email }).select('+password');
+
+  if (!user || !(await user.correctPassword(password, user.password))) {
+    return next(new AppError('Incorrect email or password', 401));
+  }
+});
