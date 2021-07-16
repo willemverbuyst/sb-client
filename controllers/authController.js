6879a4964ab1c9c57b6fcb430bf64ab063a5cc1d
Willem Verbuyst <willemverbuyst@gmail.com>
2021-07-16 18:41:32 +0200

    Create controller for sign up

--- controllers/authController.js
+++ controllers/authController.js
@@ -37,3 +37,82 @@ exports.login = catchAsync(async (req, res, next) => {
     token,
   });
 });
+
+exports.signup = catchAsync(async (req, res, next) => {
+  const {
+    userName,
+    firstName,
+    lastName,
+    email,
+    password,
+    phoneNumber,
+    admin,
+    totaalToto,
+    teamId,
+  } = req.body;
+
+  if (!req.user.admin) {
+    return next(new AppError('Je moet een admin zijn voor dit verzoek!'), 403);
+  }
+
+  if (
+    !userName ||
+    !firstName ||
+    !lastName ||
+    !email ||
+    !password ||
+    !phoneNumber ||
+    !teamId
+  ) {
+    return next(new AppError('Details ontbreken, probeer opnieuw!'), 400);
+  }
+
+  try {
+    await User.create({
+      userName,
+      firstName,
+      lastName,
+      email,
+      password: bcrypt.hashSync(password, SALT_ROUNDS),
+      phoneNumber,
+      admin,
+      totaalToto,
+      teamId,
+    });
+
+    const newUser = await User.findOne({
+      where: { email },
+      attributes: [
+        'id',
+        'userName',
+        'firstName',
+        'lastName',
+        'email',
+        'phoneNumber',
+        'admin',
+        'totaalToto',
+      ],
+      include: [{ model: Team, attributes: ['id', 'logo', 'name'] }],
+    });
+
+    res.status(201).json({
+      userData: newUser,
+      message: `Er is een nieuw account gemaakt voor ${newUser.dataValues.userName}.`,
+    });
+  } catch (error) {
+    if (error.name === 'SequelizeUniqueConstraintError')
+      return res
+        .status(400)
+        .send({ message: 'Er is al een account met dit emailadres.' });
+
+    return res.status(400).send({ message: 'Er ging iets mis, sorry.' });
+  }
+
+  const token = signToken(newUser._id);
+
+  res.status(201).json({
+    status: 'success',
+    token,
+    data: { user: newUser },
+  });
+});
--- routes/userRoutes.js
+++ routes/userRoutes.js
@@ -4,5 +4,6 @@ const authController = require('../controllers/authController');
 const router = express.Router();
 
 router.post('/login', authController.login);
+router.post('/login', authController.login);
 
 module.exports = router;
