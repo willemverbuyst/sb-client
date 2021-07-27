68cb78c0891cbfc1279eb935d8779b59474459dd
Willem Verbuyst <willemverbuyst@gmail.com>
2021-07-27 19:30:19 +0200

    Update script for seeds

--- post-release.sh
+++ post-release.sh
@@ -1,6 +1,6 @@
-npx sequelize-cli db:migrate:undo:all
-npx sequelize-cli db:migrate
-node api-football/api-calls.js --importTeams
+# npx sequelize-cli db:migrate:undo:all
+# npx sequelize-cli db:migrate
+# node api-football/api-calls.js --importTeams
 # npx sequelize-cli db:seed --seed 1-admin.js
-# npx sequelize-cli db:seed:all
+npx sequelize-cli db:seed:all
 
