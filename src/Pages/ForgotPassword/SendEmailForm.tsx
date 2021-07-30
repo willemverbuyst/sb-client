9985e1fcf0baf351f7bddf2d5eb4bd25aa18f2d4
Willem Verbuyst <willemverbuyst@gmail.com>
2021-07-30 07:10:11 +0200

    Clean up component

--- src/Pages/ForgotPassword/SendEmailForm.tsx
+++ src/Pages/ForgotPassword/SendEmailForm.tsx
@@ -1,26 +1,26 @@
-import { Link, Typography } from '@material-ui/core';
-import LockOutlinedIcon from '@material-ui/icons/LockOutlined';
-import React, { ReactElement, useState } from 'react';
-import { useDispatch } from 'react-redux';
+import { Typography } from '@material-ui/core';
+// import LockOutlinedIcon from '@material-ui/icons/LockOutlined';
+import React, { ReactElement } from 'react';
+// import { useDispatch } from 'react-redux';
 
-import AvatarIconComponent from '../../Components/Avatar/AvatarIcon';
-import SubmitButtonComponent from '../../Components/Button/SubmitButton';
-import FormContainer from '../../Components/Form/FormContainer';
-import TextFieldComponent from '../../Components/Form/TextField';
-import { requestEmailForNewPassword } from '../../store/user/action-creators';
+// import AvatarIconComponent from '../../Components/Avatar/AvatarIcon';
+// import SubmitButtonComponent from '../../Components/Button/SubmitButton';
+// import FormContainer from '../../Components/Form/FormContainer';
+// import TextFieldComponent from '../../Components/Form/TextField';
+// import { requestEmailForNewPassword } from '../../store/user/action-creators';
 
 const SendEmailForm: React.FC = (): ReactElement => {
-  const dispatch = useDispatch();
-  const [email, setEmail] = useState<string>('');
+  // const dispatch = useDispatch();
+  // const [email, setEmail] = useState<string>('');
 
-  const submitForm = (event: React.MouseEvent<HTMLButtonElement>): void => {
-    event.preventDefault();
-    dispatch(requestEmailForNewPassword(email));
-    setEmail('');
-  };
+  // const submitForm = (event: React.MouseEvent<HTMLButtonElement>): void => {
+  //   event.preventDefault();
+  //   dispatch(requestEmailForNewPassword(email));
+  //   setEmail('');
+  // };
 
-  const updateEmail = (event: React.ChangeEvent<HTMLInputElement>) =>
-    setEmail(event.target.value);
+  // const updateEmail = (event: React.ChangeEvent<HTMLInputElement>) =>
+  //   setEmail(event.target.value);
 
   return (
     // <FormContainer
