45e5892ae9f18d03a4019e90255114d62354e951
Willem Verbuyst <willemverbuyst@gmail.com>
2021-08-04 05:11:36 +0200

    Test validate new password

---
+++ validators/validateNewPassword.test.js
@@ -0,0 +1,34 @@
+const bcrypt = require('bcrypt');
+const validateNewPassword = require('./validateNewPassword');
+
+describe('validateNewPassword function', () => {
+  test('returns false given an empty string', () => {
+    expect(validateNewPassword('')).toBe(false);
+  });
+
+  test('returns false given a string and an empty string', () => {
+    expect(validateNewPassword('newPassword', '')).toBe(false);
+  });
+
+  test('returns false given arguments are not strings', () => {
+    expect(validateNewPassword(123, 123)).toBe(false);
+  });
+
+  test('returns false given arguments are the same', () => {
+    expect(
+      validateNewPassword(
+        'currentPassword',
+        bcrypt.hashSync('currentPassword', Number(process.env.SALT_ROUNDS)),
+      ),
+    ).toBe(false);
+  });
+
+  test('returns true given arguments are not the same', () => {
+    expect(
+      validateNewPassword(
+        'newPassword',
+        bcrypt.hashSync('currentPassword', Number(process.env.SALT_ROUNDS)),
+      ),
+    ).toBe(true);
+  });
+});
