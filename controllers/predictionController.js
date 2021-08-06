f6b2d1004918830247cca8368ac8553208a22154
Willem Verbuyst <willemverbuyst@gmail.com>
2021-08-06 05:59:11 +0200

    Update error handling predictionController

--- controllers/predictionController.js
+++ controllers/predictionController.js
@@ -16,8 +16,17 @@ exports.getAllPredictions = catchAsync(async (req, res, next) => {
   const playerId = req.params.id;
   const userId = req.user.dataValues.id;
 
-  const fixtures = await getAllFixturesWithPrediction(playerId, userId);
   const { userName } = await getUserById(playerId);
+
+  if (!userName) {
+    return next(new AppError('No player found with this id!', 404));
+  }
+
+  const fixtures = await getAllFixturesWithPrediction(playerId, userId);
+
+  if (!fixtures) {
+    return next(new AppError('No predictions found', 404));
+  }
 
   res.status(200).json({
     status: 'success',
@@ -35,15 +44,16 @@ exports.postPrediction = catchAsync(async (req, res, next) => {
   const { pGoalsHomeTeam, pGoalsAwayTeam } = req.body;
 
   if (!validatePredictionInput(pGoalsHomeTeam, pGoalsAwayTeam, fixtureId)) {
-    return next(new AppError('Details ontbreken, probeer opnieuw!', 404));
+    return next(new AppError('Missing details, please try again!', 404));
   }
 
   const fixture = await getFixture(fixtureId);
 
+  // TODO: BUILD TIME GUARD
   if (!validateFixtureStatus(fixture.status, next)) {
     return next(
       new AppError(
-        'Je kan de uitslag van een afgelopen wedstrijd niet wijzigen!',
+        'You cannot post a prediction of a match that has finsihed already!',
         404,
       ),
     );
@@ -61,7 +71,7 @@ exports.postPrediction = catchAsync(async (req, res, next) => {
     data: {
       prediction,
     },
-    message: 'Je voorspelling is geplaatst.',
+    message: 'You have posted a prediction.',
   });
 });
 
@@ -76,10 +86,11 @@ exports.updatePrediction = catchAsync(async (req, res, next) => {
 
   const fixture = await getFixture(fixtureId);
 
+  // TODO: BUILD TIME GUARD
   if (!validateFixtureStatus(fixture.status, next)) {
     return next(
       new AppError(
-        'Je kan de uitslag van een afgelopen wedstrijd niet wijzigen!',
+        'You cannot change a prediction of a match that has finsihed already!',
         404,
       ),
     );
@@ -97,6 +108,6 @@ exports.updatePrediction = catchAsync(async (req, res, next) => {
     data: {
       prediction,
     },
-    message: 'Je voorspelling is aangepast.',
+    message: 'Your prediction has been updated.',
   });
 });
