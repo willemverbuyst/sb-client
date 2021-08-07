629c059e024a8bd225301f3cc63b48056eaf80ac
Willem Verbuyst <willemverbuyst@gmail.com>
2021-08-07 08:15:26 +0200

    Customize ticks on chart for fixture

--- src/Pages/Scores/Fixture/ScoresFixtureBarChart.tsx
+++ src/Pages/Scores/Fixture/ScoresFixtureBarChart.tsx
@@ -64,6 +64,8 @@ const ScoresForFixtureBarChart: React.FC<IProps> = ({
     ],
   };
 
+  const ticks = [1, 2, 3, '', 5, '', 7, 8, 9, 10];
+
   const chartOptions: chartjs.ChartOptions = {
     tooltips: {
       enabled: false,
@@ -90,10 +92,27 @@ const ScoresForFixtureBarChart: React.FC<IProps> = ({
           position: 'top',
           stacked: true,
           ticks: {
+            autoSkip: false,
             display: true,
+            fontStyle: 'bold',
             suggestedMin: 0,
             suggestedMax: 10,
+            stepSize: 1,
+            callback: (value, _index, _labels) => {
+              if (
+                value === 0 ||
+                value === 2 ||
+                value === 5 ||
+                value === 7 ||
+                value === 10
+              ) {
+                return value;
+              } else {
+                return '';
+              }
+            },
           },
+
           gridLines: {
             display: false,
           },
