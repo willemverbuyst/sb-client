ed47d819ce885eb334e0b7eb3eec44c27fd5b907
Willem Verbuyst <willemverbuyst@gmail.com>
2021-08-07 05:18:36 +0200

    Update list of players page

--- src/Pages/Players/ListOfPlayers/index.tsx
+++ src/Pages/Players/ListOfPlayers/index.tsx
@@ -1,15 +1,18 @@
+import { Box } from '@material-ui/core';
 import React, { ReactElement, useEffect } from 'react';
 import { useDispatch, useSelector } from 'react-redux';
 
 import MessageComponent from '../../../Components/Communication/Message';
+import ProgressComponent from '../../../Components/Progress';
 import PageTitle from '../../../Components/Title/PageTitle';
-import PageContent from '../../../Sections/PageContent';
+import { selectAppLoading } from '../../../store/appState/selectors';
 import { fetchAllPlayers } from '../../../store/players/action-creators';
 import { selectPlayersSortedByName } from '../../../store/players/selectors';
 import TableWithPlayers from './TableWithPlayers';
 
 const ListOfPlayers: React.FC = (): ReactElement => {
   const dispatch = useDispatch();
+  const isLoading = useSelector(selectAppLoading);
   const playersSortedByName = useSelector(selectPlayersSortedByName);
 
   useEffect(() => {
@@ -19,19 +22,16 @@ const ListOfPlayers: React.FC = (): ReactElement => {
   }, [dispatch, playersSortedByName]);
 
   return (
-    <PageContent
-      loadingText="Spelers"
-      content={
-        playersSortedByName ? (
-          <>
-            <PageTitle title="Spelers" color="secondary" />
-            <TableWithPlayers playersSortedByName={playersSortedByName} />
-          </>
-        ) : (
-          <MessageComponent message="Geen spelers gevonden" />
-        )
-      }
-    />
+    <Box>
+      <PageTitle title="Spelers" color="secondary" />
+      {isLoading ? (
+        <ProgressComponent />
+      ) : playersSortedByName ? (
+        <TableWithPlayers playersSortedByName={playersSortedByName} />
+      ) : (
+        <MessageComponent message="Geen spelers gevonden" />
+      )}
+    </Box>
   );
 };
 
