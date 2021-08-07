43fc2a7e36351c5ce9b5f4de13fce41d5b422b51
Willem Verbuyst <willemverbuyst@gmail.com>
2021-08-07 07:45:49 +0200

    Add guard to signup page

--- src/Pages/SignUp/index.tsx
+++ src/Pages/SignUp/index.tsx
@@ -6,10 +6,11 @@ import { useHistory } from 'react-router-dom';
 import MessageComponent from '../../Components/Communication/Message';
 import ProgressComponent from '../../Components/Progress';
 import PageTitle from '../../Components/Title/PageTitle';
+import Guard from '../../Sections/Guard';
 import { selectAppLoading } from '../../store/appState/selectors';
 import { fetchAllTeams } from '../../store/teams/action-creators';
 import { selectTeams } from '../../store/teams/selectors';
-import { selectUser } from '../../store/user/selectors';
+import { selectToken, selectUser } from '../../store/user/selectors';
 import SignUpForm from './SingUpForm';
 
 const SignUp: React.FC = (): ReactElement => {
@@ -17,29 +18,34 @@ const SignUp: React.FC = (): ReactElement => {
   const history = useHistory();
   const isLoading = useSelector(selectAppLoading);
   const teams = useSelector(selectTeams);
+  const token = useSelector(selectToken);
   const user = useSelector(selectUser);
 
   useEffect(() => {
-    if (!teams) {
+    if (token && !teams) {
       dispatch(fetchAllTeams());
     }
-  }, [dispatch, teams]);
+  }, [dispatch, teams, token]);
 
   useEffect(() => {
     if (user && !user.admin) history.push('/page-not-found');
   });
 
   return (
-    <Box>
-      <PageTitle title="Sign up" color="secondary" />
-      {isLoading ? (
-        <ProgressComponent />
-      ) : teams ? (
-        <SignUpForm teams={teams} />
-      ) : (
-        <MessageComponent message="Geen teams gevonden voor de signup, probeer het later nog eens" />
-      )}
-    </Box>
+    <Guard
+      content={
+        <Box>
+          <PageTitle title="Sign up" color="secondary" />
+          {isLoading ? (
+            <ProgressComponent />
+          ) : teams ? (
+            <SignUpForm teams={teams} />
+          ) : (
+            <MessageComponent message="Geen teams gevonden voor de signup, probeer het later nog eens" />
+          )}
+        </Box>
+      }
+    />
   );
 };
 
