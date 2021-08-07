9ffe64ba47648b8e769189171c573c593e47e095
Willem Verbuyst <willemverbuyst@gmail.com>
2021-08-07 04:33:29 +0200

    Update total toto page

--- src/Pages/Scores/TotalToto/index.tsx
+++ src/Pages/Scores/TotalToto/index.tsx
@@ -1,45 +1,39 @@
-import React, { ReactElement, useEffect, useState } from 'react';
+import { Box } from '@material-ui/core';
+import React, { ReactElement, useEffect } from 'react';
 import { useDispatch, useSelector } from 'react-redux';
 
 import MessageComponent from '../../../Components/Communication/Message';
+import ProgressComponent from '../../../Components/Progress';
 import PageTitle from '../../../Components/Title/PageTitle';
 import ScoresBarChart from '../../../Sections/Charts/ScoresBarChart';
-import PageContent from '../../../Sections/PageContent';
+import { selectAppLoading } from '../../../store/appState/selectors';
 import { fetchScoresTotalToto } from '../../../store/scores/action-creators';
 import { selectScoresTotalTotoSortedByScore } from '../../../store/scores/selectors';
 
 const TotalToto: React.FC = (): ReactElement => {
   const dispatch = useDispatch();
+  const isLoading = useSelector(selectAppLoading);
   const scoresTotalTotoSortedByScore = useSelector(
     selectScoresTotalTotoSortedByScore,
   );
-  const [message, setMessage] = useState('');
 
   useEffect(() => {
     if (!scoresTotalTotoSortedByScore) {
-      (async () => {
-        await dispatch(fetchScoresTotalToto());
-        if (!scoresTotalTotoSortedByScore) {
-          setMessage('Nog geen scores voor de totaalToto');
-        }
-      })();
+      dispatch(fetchScoresTotalToto());
     }
   }, [dispatch, scoresTotalTotoSortedByScore]);
 
   return (
-    <PageContent
-      loadingText=""
-      content={
-        <>
-          <PageTitle title="Totaaltoto" color="secondary" />
-          {scoresTotalTotoSortedByScore ? (
-            <ScoresBarChart scores={scoresTotalTotoSortedByScore} />
-          ) : (
-            <MessageComponent message={message} />
-          )}
-        </>
-      }
-    />
+    <Box>
+      <PageTitle title="Totaaltoto" color="secondary" />
+      {isLoading ? (
+        <ProgressComponent />
+      ) : scoresTotalTotoSortedByScore ? (
+        <ScoresBarChart scores={scoresTotalTotoSortedByScore} />
+      ) : (
+        <MessageComponent message="Nog geen scores voor de totaalToto" />
+      )}
+    </Box>
   );
 };
 
