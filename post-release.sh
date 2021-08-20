e342b84533a8c0bdd2df66235ff471cf728882c5
Willem Verbuyst <willemverbuyst@gmail.com>
2021-08-20 16:51:23 +0200

    Turn off post release scripts

--- post-release.sh
+++ post-release.sh
@@ -1,7 +1,7 @@
-npx sequelize-cli db:migrate:undo:all
-npx sequelize-cli db:migrate
-node api-football/api-calls.js --importTeams
-node api-football/api-calls.js --importFixtures
+# npx sequelize-cli db:migrate:undo:all
+# npx sequelize-cli db:migrate
+# node api-football/api-calls.js --importTeams
+# node api-football/api-calls.js --importFixtures
 # npx sequelize-cli db:seed --seed 1-admin.js
-npx sequelize-cli db:seed:all
+# npx sequelize-cli db:seed:all
 
