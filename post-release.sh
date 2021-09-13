b586bc137dc88f0ede2e544205cd948f265e73b3
Willem Verbuyst <willemverbuyst@gmail.com>
2021-09-13 19:20:37 +0200

    Prepare for heroku dev

--- post-release.sh
+++ post-release.sh
@@ -1,10 +1,10 @@
-# npx sequelize-cli db:migrate:undo:all
-# npx sequelize-cli db:migrate
-# node api-football/api-calls.js --importTeams
-# node api-football/api-calls.js --importFixtures
+npx sequelize-cli db:migrate:undo:all
+npx sequelize-cli db:migrate
+node api-football/api-calls.js --importTeams
+node api-football/api-calls.js --importFixtures
 # npx sequelize-cli db:seed --seed 1-dummy-admin.js
-# npx sequelize-cli db:seed --seed 2-dummy-users.js
-# npx sequelize-cli db:seed --seed 3-dummy-predictions.js
+npx sequelize-cli db:seed --seed 2-dummy-users.js
+npx sequelize-cli db:seed --seed 3-dummy-predictions.js
 # npx sequelize-cli db:seed:all
 
 
