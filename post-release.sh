23166192872fc7e69c0e9b1d0385bc402c369065
Willem Verbuyst <willemverbuyst@gmail.com>
2021-09-13 19:45:51 +0200

    Adjust script for deploy heroku

--- post-release.sh
+++ post-release.sh
@@ -3,8 +3,8 @@ npx sequelize-cli db:migrate
 node api-football/api-calls.js --importTeams
 node api-football/api-calls.js --importFixtures
 # npx sequelize-cli db:seed --seed 1-dummy-admin.js
-npx sequelize-cli db:seed --seed 2-dummy-users.js
-npx sequelize-cli db:seed --seed 3-dummy-predictions.js
+# npx sequelize-cli db:seed --seed 2-dummy-users.js
+# npx sequelize-cli db:seed --seed 3-dummy-predictions.js
 # npx sequelize-cli db:seed:all
 
 
