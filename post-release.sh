9fd5c4c9b4d76ff2527e28ff7789b245981776ef
Willem Verbuyst <willemverbuyst@gmail.com>
2021-09-13 18:47:50 +0200

    Fix typo scripts

--- post-release.sh
+++ post-release.sh
@@ -3,7 +3,7 @@
 # node api-football/api-calls.js --importTeams
 # node api-football/api-calls.js --importFixtures
 # npx sequelize-cli db:seed --seed 1-dummy-admin.js
-# npx sequelize-cli db:seed --seed 2-dummy-usrs.js
+# npx sequelize-cli db:seed --seed 2-dummy-users.js
 # npx sequelize-cli db:seed --seed 3-dummy-predictions.js
 # npx sequelize-cli db:seed:all
 
