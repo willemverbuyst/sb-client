2882ae36d5c8ce2a9b484dd8b5184d604996638b
Willem Verbuyst <willemverbuyst@gmail.com>
2021-10-23 12:42:46 +0200

    Test isValidFixtureId

--- src/utils/helpers/getLastMonday.helpers.js
+++ src/utils/helpers/getLastMonday.helpers.js
@@ -1,6 +1,11 @@
+const getTodayAsNumber = () => new Date().getDay() || 7;
+
+// const getDays = (todayAsNumber) => (todayAsNumber - 1) * 24 * 60 * 60;
+
 module.exports = () => {
   const date = new Date();
-  const day = date.getDay() || 7;
-  if (day !== 1) return Math.floor(date / 1000) - 24 * (day - 1) * 60 * 60;
+  const todayAsNumber = getTodayAsNumber();
+  if (todayAsNumber !== 1)
+    return Math.floor(date / 1000) - 24 * (todayAsNumber - 1) * 60 * 60;
   return Math.floor(date / 1000);
 };
