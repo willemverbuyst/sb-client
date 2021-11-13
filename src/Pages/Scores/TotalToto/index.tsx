866b3a986944c7b3a255c126736243a0b7c2b4fa
Willem Verbuyst <willemverbuyst@gmail.com>
2021-11-13 11:12:13 +0100

    Separate logic for scores totaltoto

--- src/Pages/Scores/TotalToto/index.tsx
+++ src/Pages/Scores/TotalToto/index.tsx
@@ -2,15 +2,13 @@ import { Box } from '@material-ui/core'
 import React, { ReactElement, useEffect } from 'react'
 import { useDispatch, useSelector } from 'react-redux'
 
-import MessageComponent from '../../../Components/Communication/Message'
-import ProgressComponent from '../../../Components/Progress'
 import PageTitle from '../../../Components/Title/PageTitle'
-import ScoresBarChart from '../../../Sections/Charts/ScoresBarChart'
 import Guard from '../../../Sections/Guard'
 import { selectAppLoading } from '../../../store/appState/selectors'
 import { fetchScoresTotalToto } from '../../../store/scores/action-creators'
 import { selectScoresTotalTotoSortedByScore } from '../../../store/scores/selectors'
 import { selectToken } from '../../../store/user/selectors'
+import renderDisplay from './display'
 
 const TotalToto: React.FC = (): ReactElement => {
   const dispatch = useDispatch()
@@ -31,13 +29,7 @@ const TotalToto: React.FC = (): ReactElement => {
       content={
         <Box>
           <PageTitle title="Totaaltoto" color="secondary" />
-          {isLoading ? (
-            <ProgressComponent />
-          ) : scoresTotalTotoSortedByScore ? (
-            <ScoresBarChart scores={scoresTotalTotoSortedByScore} />
-          ) : (
-            <MessageComponent message="Nog geen scores voor de totaalToto" />
-          )}
+          {renderDisplay({ isLoading, scoresTotalTotoSortedByScore })}
         </Box>
       }
     />
