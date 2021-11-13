69b81345393aa0abefba19a1e555e06f95474fbf
Willem Verbuyst <willemverbuyst@gmail.com>
2021-11-13 11:20:30 +0100

    Separate logic for scores totoround

--- src/Pages/Scores/TotoRound/index.tsx
+++ src/Pages/Scores/TotoRound/index.tsx
@@ -3,10 +3,7 @@ import React, { ReactElement, useEffect } from 'react'
 import { useDispatch, useSelector } from 'react-redux'
 import { useParams } from 'react-router-dom'
 
-import MessageComponent from '../../../Components/Communication/Message'
-import ProgressComponent from '../../../Components/Progress'
 import PageTitle from '../../../Components/Title/PageTitle'
-import ScoresBarChart from '../../../Sections/Charts/ScoresBarChart'
 import Guard from '../../../Sections/Guard'
 import { selectAppLoading } from '../../../store/appState/selectors'
 import { fetchScoresTotoRound } from '../../../store/scores/action-creators'
@@ -15,7 +12,7 @@ import {
   selectTotoRoundId,
 } from '../../../store/scores/selectors'
 import { selectToken } from '../../../store/user/selectors'
-import Pagination from './Pagination'
+import renderDisplay from './display'
 
 const TotoRound: React.FC = (): ReactElement => {
   const dispatch = useDispatch()
@@ -39,19 +36,7 @@ const TotoRound: React.FC = (): ReactElement => {
       content={
         <Box>
           <PageTitle title={`Totoronde ${totoRound}`} color="secondary" />
-          {isLoading ? (
-            <ProgressComponent />
-          ) : scoresRoundSortedByScore && scoresRoundSortedByScore.length ? (
-            <>
-              <Pagination totoRound={totoRound} />
-              <ScoresBarChart scores={scoresRoundSortedByScore} />
-            </>
-          ) : (
-            <>
-              <MessageComponent message="Nog geen scores voor totoronde" />
-              <Pagination totoRound={totoRound} />
-            </>
-          )}
+          {renderDisplay({ isLoading, totoRound, scoresRoundSortedByScore })}
         </Box>
       }
     />
