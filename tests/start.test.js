7eecbb3624c7a24909e4ccb104ce6ba9ddde39e6
Willem Verbuyst <willemverbuyst@gmail.com>
2022-01-21 20:04:56 +0100

    Try small changes

--- tests/start.test.js
+++ tests/start.test.js
@@ -2,10 +2,17 @@ const puppeteer = require('puppeteer')
 
 describe('My first puppeteer test', () => {
 	it('should load the browser', async () => {
-		const browser = await puppeteer.launch({ headless: false })
+		const browser = await puppeteer.launch({
+			headless: false,
+			slowMo: 10,
+			devtools: false,
+		})
 		const page = await browser.newPage()
 
 		await page.goto('http://localhost:3000/')
+		await page.waitForSelector('h4')
+		await page.reload()
+		await page.waitForTimeout(2000)
 		await page.waitForSelector('h4')
 		await browser.close()
 	})
