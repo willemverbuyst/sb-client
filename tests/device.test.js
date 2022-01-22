dc0b1340d3d8b8a25dfd3830c130fc2da6fb385e
Willem Verbuyst <willemverbuyst@gmail.com>
2022-01-22 05:40:12 +0100

    Test different devices

---
+++ tests/device.test.js
@@ -0,0 +1,39 @@
+const puppeteer = require('puppeteer')
+
+describe('Application ', () => {
+	let browser
+	let page
+
+	beforeEach(async () => {
+		browser = await puppeteer.launch({
+			headless: false,
+			slowMo: 10,
+			devtools: false,
+		})
+		page = await browser.newPage()
+	})
+
+	afterEach(async () => {
+		await browser.close()
+	})
+
+	it('should be visible on desktop', async () => {
+		await page.setViewport({ width: 1650, height: 1050 })
+		await page.goto('http://localhost:3000/')
+		await page.waitForTimeout(2000)
+	})
+
+	it('should be visible on tablet', async () => {
+		const tablet = puppeteer.devices['iPad landscape']
+		await page.emulate(tablet)
+		await page.goto('http://localhost:3000/')
+		await page.waitForTimeout(2000)
+	})
+
+	it('should be visible on mobile', async () => {
+		const mobile = puppeteer.devices['iPhone X']
+		await page.emulate(mobile)
+		await page.goto('http://localhost:3000/')
+		await page.waitForTimeout(2000)
+	})
+})
