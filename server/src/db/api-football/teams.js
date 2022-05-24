f5a5811c55b69a25db6750227d440d8fdc0bfdd8
Willem Verbuyst <willemverbuyst@gmail.com>
2022-05-24 19:54:38 +0200

    Work on teams endpoint

--- server/src/db/api-football/teams.js
+++ server/src/db/api-football/teams.js
@@ -1,29 +1,31 @@
 const Axios = require('axios');
-const Team = require('../models').team;
+// const Team = require('../models').team;
 
 const getTeams = async () => {
   const response = await Axios.get(
     `${process.env.API_URL}/teams/search/Netherlands`,
-    {
-      headers: {
-        'X-RapidAPI-Key': process.env.API_KEY,
-      },
-    },
+    // {
+    //   headers: {
+    //     'X-RapidAPI-Key': process.env.API_KEY,
+    //   },
+    // },
   );
 
   const allTeams = response.data.api;
 
-  const teams = allTeams.teams.map((team) => ({
-    id: team.team_id,
-    name: team.name,
-    logo: team.logo,
-  }));
+  console.log('allTeams', allTeams);
 
-  // console.log(teams);
+  // const teams = allTeams.teams.map((team) => ({
+  //   id: team.team_id,
+  //   name: team.name,
+  //   logo: team.logo,
+  // }));
 
-  await Team.bulkCreate(teams, {
-    updateOnDuplicate: ['id'],
-  });
+  // // console.log(teams);
+
+  // await Team.bulkCreate(teams, {
+  //   updateOnDuplicate: ['id'],
+  // });
 };
 
 module.exports = getTeams;
--- service/controllers/teams.py
+++ service/controllers/teams.py
@@ -5,7 +5,7 @@ from business.teams import get_teams_from_json
 teams_router = APIRouter()
 
 
-@teams_router.get("/teams", tags=["teams"])
+@teams_router.get("/teams/search/Netherlands", tags=["teams"])
 async def get_teams() -> dict:
     teams = get_teams_from_json()
-    return teams
+    return {"api": teams}
