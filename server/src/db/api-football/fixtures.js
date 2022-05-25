b34a89cfc0ef2200ef00eda605f78952bde393b1
Willem Verbuyst <willemverbuyst@gmail.com>
2022-05-25 04:53:44 +0200

    Import fixtures from service

--- server/src/db/api-football/fixtures.js
+++ server/src/db/api-football/fixtures.js
@@ -7,49 +7,49 @@ const firstFixture = process.env.ID_FIRST_FIXTURE;
 const updateInterval = process.env.UPDATE_INTERVAL;
 
 const getFixtures = async () => {
-  const lastUpdate = await getLastUpdateQuery(firstFixture);
+  // const lastUpdate = await getLastUpdateQuery(firstFixture);
   const rightNow = new Date();
 
-  if (!lastUpdate || rightNow - lastUpdate > updateInterval) {
-    const response = await Axios.get(
-      `${process.env.API_URL}/fixtures/league/${process.env.LEAGUE_ID}`,
-      {
-        headers: {
-          'X-RapidAPI-Key': process.env.API_KEY,
-        },
-      },
-    );
+  // if (!lastUpdate || rightNow - lastUpdate > updateInterval) {
+  const response = await Axios.get(
+    `${process.env.API_URL}/fixtures/league/${process.env.LEAGUE_ID}`,
+    // {
+    //   headers: {
+    //     'X-RapidAPI-Key': process.env.API_KEY,
+    //   },
+    // },
+  );
 
-    const fixtureData = response.data.api;
+  const fixtureData = response.data.api;
 
-    const fixtures = fixtureData.fixtures.map((fixture) => ({
-      id: +fixture.fixture_id,
-      homeTeamId: fixture.homeTeam.team_id,
-      homeTeamName: fixture.homeTeam.team_name,
-      homeTeamLogo: fixture.homeTeam.logo,
-      goalsHomeTeam: fixture.goalsHomeTeam,
-      awayTeamId: fixture.awayTeam.team_id,
-      awayTeamName: fixture.awayTeam.team_name,
-      awayTeamLogo: fixture.awayTeam.logo,
-      goalsAwayTeam: fixture.goalsAwayTeam,
-      eventTimeStamp: fixture.event_timestamp,
-      round: fixture.round,
-      status: fixture.status,
-      updateAt: rightNow,
-    }));
+  const fixtures = fixtureData.fixtures.map((fixture) => ({
+    id: +fixture.fixture_id,
+    homeTeamId: fixture.homeTeam.team_id,
+    homeTeamName: fixture.homeTeam.team_name,
+    homeTeamLogo: fixture.homeTeam.logo,
+    goalsHomeTeam: fixture.goalsHomeTeam,
+    awayTeamId: fixture.awayTeam.team_id,
+    awayTeamName: fixture.awayTeam.team_name,
+    awayTeamLogo: fixture.awayTeam.logo,
+    goalsAwayTeam: fixture.goalsAwayTeam,
+    eventTimeStamp: fixture.event_timestamp,
+    round: fixture.round,
+    status: fixture.status,
+    updateAt: rightNow,
+  }));
 
-    // console.log(fixtures);
+  console.log(fixtures);
 
-    await Fixture.bulkCreate(fixtures, {
-      updateOnDuplicate: [
-        'goalsHomeTeam',
-        'goalsAwayTeam',
-        'status',
-        'eventTimeStamp',
-        'updatedAt',
-      ],
-    });
-  }
+  // await Fixture.bulkCreate(fixtures, {
+  //   updateOnDuplicate: [
+  //     'goalsHomeTeam',
+  //     'goalsAwayTeam',
+  //     'status',
+  //     'eventTimeStamp',
+  //     'updatedAt',
+  //   ],
+  // });
 };
+// };
 
 module.exports = getFixtures;
--- service/controllers/fixtures.py
+++ service/controllers/fixtures.py
@@ -1,11 +1,15 @@
 from fastapi import APIRouter
-
+from dotenv import load_dotenv
+import os
 from business.fixtures import get_fixtures_from_json
+load_dotenv()
 
 fixtures_router = APIRouter()
 
+league_id = os.getenv("LEAGUE_ID")
 
-@fixtures_router.get("/fixtures", tags=["fixtures"])
+
+@fixtures_router.get(f"/fixtures/league/{league_id}", tags=["fixtures"])
 async def get_fixtures() -> dict:
-    teams = get_fixtures_from_json()
-    return teams
+    fixtures = get_fixtures_from_json()
+    return {"api": {"fixtures": fixtures}}
