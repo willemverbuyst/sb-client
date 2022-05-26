f3c4879a39bb57f78699e267d831f9fe75be48dd
Willem Verbuyst <willemverbuyst@gmail.com>
2022-05-26 16:06:28 +0200

    Spin up  server

--- chat/src/index.ts
+++ chat/src/index.ts
@@ -1,1 +1,10 @@
-console.log('hello world');
+import express, { Application, Request, Response, NextFunction } from 'express'
+
+const app: Application = express()
+const port = 9000
+
+app.use('/', (_req: Request, res: Response, _next: NextFunction) => {
+  res.status(200).send({ message: 'Testing root' })
+})
+
+app.listen(port, () => console.log(`Server is listening on port ${port}!`))
