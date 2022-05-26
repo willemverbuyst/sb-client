1ed6ff2f8cff0b012332774ae61cf046589894cf
Willem Verbuyst <willemverbuyst@gmail.com>
2022-05-26 16:27:29 +0200

    Setup socketio on server

--- chat/src/index.ts
+++ chat/src/index.ts
@@ -1,4 +1,5 @@
 import express, { Application, Request, Response, NextFunction } from 'express'
+import { Server } from 'socket.io'
 
 const app: Application = express()
 const port = 9000
@@ -7,4 +8,17 @@ app.use('/', (_req: Request, res: Response, _next: NextFunction) => {
   res.status(200).send({ message: 'Testing root' })
 })
 
-app.listen(port, () => console.log(`Server is listening on port ${port}!`))
+const expressServer = app.listen(port, () =>
+  console.log(`Server is listening on port ${port}!`)
+)
+
+const io = new Server(expressServer)
+
+io.on('connection', socket => {
+  socket.emit('messageFromServer', {
+    message: 'This is comming from the server',
+  })
+  socket.on('messageFromClient', messageFromClient => {
+    console.log(messageFromClient)
+  })
+})
