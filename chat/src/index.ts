c670e9b951893d2efcdf4a56ad40b61a7981c1e2
Willem Verbuyst <willemverbuyst@gmail.com>
2022-05-27 05:32:12 +0200

    Handle cors issue

--- chat/src/index.ts
+++ chat/src/index.ts
@@ -1,8 +1,11 @@
 import express, { Application, Request, Response, NextFunction } from 'express'
 import { Server } from 'socket.io'
+import corsMiddleware from 'cors'
 
 const app: Application = express()
 const port = 9000
+
+app.use(corsMiddleware)
 
 app.use('/', (_req: Request, res: Response, _next: NextFunction) => {
   res.status(200).send({ message: 'Testing root' })
@@ -12,11 +15,11 @@ const expressServer = app.listen(port, () =>
   console.log(`Server is listening on port ${port}`)
 )
 
-const io = new Server(expressServer)
+const io = new Server(expressServer, { cors: { origin: '*' } })
 
 io.on('connection', socket => {
   socket.emit('messageFromServer', {
-    message: 'This is comming from the server',
+    message: 'This is a message from the server',
   })
   socket.on('messageFromClient', messageFromClient => {
     console.log(messageFromClient)
