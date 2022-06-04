817bbb9f727c153333bfa31655df826fa2f35a44
Willem Verbuyst <willemverbuyst@gmail.com>
2022-06-04 11:36:37 +0200

    Use icons for private or not

--- chat/src/data/namespaces.ts
+++ chat/src/data/namespaces.ts
@@ -26,14 +26,14 @@ namespaces.push(wikiNs, mozNs, linuxNs)
 // Make the main room and add it to rooms. it will ALWAYS be 0
 wikiNs.addRoom(new Room(0, 'New Articles', 'Wiki'))
 wikiNs.addRoom(new Room(1, 'Editors', 'Wiki'))
-wikiNs.addRoom(new Room(2, 'Other', 'Wiki'))
+wikiNs.addRoom(new Room(2, 'Other', 'Wiki', true))
 
-mozNs.addRoom(new Room(0, 'Firefox', 'Mozilla'))
+mozNs.addRoom(new Room(0, 'Firefox', 'Mozilla', true))
 mozNs.addRoom(new Room(1, 'SeaMonkey', 'Mozilla'))
 mozNs.addRoom(new Room(2, 'SpiderMonkey', 'Mozilla'))
 mozNs.addRoom(new Room(3, 'Rust', 'Mozilla'))
 
-linuxNs.addRoom(new Room(0, 'Debian', 'Linux'))
+linuxNs.addRoom(new Room(0, 'Debian', 'Linux', true))
 linuxNs.addRoom(new Room(1, 'Red Hat', 'Linux'))
 linuxNs.addRoom(new Room(2, 'MacOs', 'Linux'))
 linuxNs.addRoom(new Room(3, 'Kernal Development', 'Linux'))
