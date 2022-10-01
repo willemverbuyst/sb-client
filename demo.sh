5385725a00b3275165ec813744380aa6976c06d0
Willem Verbuyst <willemverbuyst@gmail.com>
2022-10-01 17:19:14 +0200

    Create bash script for demo (pt.1)

---
+++ demo.sh
@@ -0,0 +1,42 @@
+echo "Welcome to the toto game"
+
+read -p "Do you want to see the demo [y/n]? " -n 1 -r
+echo
+if [[ ! $REPLY =~ ^[Yy]$ ]]
+then
+    exit 0
+fi
+
+echo "We will create an account for you so you can log in"
+
+read -p "Enter a username: " USERNAME
+read -p "Enter a first name: " FIRSTNAME
+read -p "Enter a last name: " LASTNAME
+read -p "Enter an email address: " EMAIL 
+read -p "Enter a password: " PASSWORD 
+
+
+echo "Update file with demo user"
+NEW_FILE="./server/src/db/dummy-data/demoUser.json"
+touch $NEW_FILE
+
+echo "{
+  \"userName\": \"$USERNAME\",
+  \"firstName\": \"$FIRSTNAME\",
+  \"lastName\": \"$LASTNAME\",
+  \"email\": \"$EMAIL\",
+  \"password\": \"$PASSWORD\"
+}" > $NEW_FILE
+
+echo "Spinning up the service for dummy soccer data"
+echo "Spinning up the postgress database"
+
+docker-compose up -d
+
+echo "Starting go service for rules"
+
+
+
+
+
+# exit 0
